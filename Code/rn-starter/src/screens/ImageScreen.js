d5b24e6467acf081cdc29d641549bd2ac3b1bea7
Andrea Cagnetta <andrea.cagnetta@vidiemme.it>
2024-01-18 17:34:44 +0100

    Add image to ImageScreen & CounterScreen component

--- Code/rn-starter/src/screens/ImageScreen.js
+++ Code/rn-starter/src/screens/ImageScreen.js
@@ -1,16 +1,29 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import ImageDetail from "../components/ImageDetail";
 
 const ImageScreen = () => {
     return (
-        <Text>Image Screen</Text>
+        <View>
+            <ImageDetail 
+                title="Forest" 
+                imageSource={require("../../assets/forest.jpg")}
+                score={9}
+            />
+            <ImageDetail 
+                title="Beach" 
+                imageSource={require("../../assets/beach.jpg")}
+                score={7}
+            />
+            <ImageDetail 
+                title="Mountain" 
+                imageSource={require("../../assets/mountain.jpg")}
+                score={10}
+            />
+        </View>
     );
 };
 
-const styles = StyleSheet.create({
-    text: {
-      fontSize: 30,
-    },
-  });
+const styles = StyleSheet.create({});
   
-  export default ImageScreen;
+export default ImageScreen;

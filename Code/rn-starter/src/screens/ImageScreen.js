b61dc9d12803cfe7f0ce8dc79ca324a5f9361e8f
Andrea Cagnetta <andrea.cagnetta@vidiemme.it>
2024-01-18 17:12:10 +0100

    Add ImageScreen component

---
+++ Code/rn-starter/src/screens/ImageScreen.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const ImageScreen = () => {
+    return (
+        <Text>Image Screen</Text>
+    );
+};
+
+const styles = StyleSheet.create({
+    text: {
+      fontSize: 30,
+    },
+  });
+  
+  export default ImageScreen;

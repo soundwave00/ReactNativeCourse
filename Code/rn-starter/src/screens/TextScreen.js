98a9085f83963ddfd1910a6873243a34694d99c2
Andrea Cagnetta <andrea.cagnetta@vidiemme.it>
2024-02-01 17:32:09 +0100

    Ad text screen

---
+++ Code/rn-starter/src/screens/TextScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text, StyleSheet } from 'react-native';
+
+const TextScreen = () => {
+    return <View>
+        <Text>text screen</Text>
+    </View>
+};
+
+const styles = StyleSheet.create({});
+
+export default TextScreen;
